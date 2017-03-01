8a4ae22883abcb9287b3c2374d417973c57253cf
Carl <ldscode@163.com>
2017-03-01 21:09:13 +0800

    添加注释

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// 加载依赖库, Express3 中这个类库都封装在connect 中, 现在需要单独地加载
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,34 +6,41 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// 加载路由控制
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// 创建项目实例
 var app = express();
 
-// view engine setup
+// view engine setup 定义jade模板引擎和模板文件位置,也可以使用ejs或其他模板引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing your favicon in /public 定义icon图标
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// 定义日志和输出级别
 app.use(logger('dev'));
+// 定义数据解析器
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// 定义cookie解析器
 app.use(cookieParser());
+// 定义静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 匹配路径和路由
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 404错误处理
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// error handler 开发环境, 500错误处理和错误堆栈跟踪
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -43,4 +51,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// 输出模型app
 module.exports = app;
